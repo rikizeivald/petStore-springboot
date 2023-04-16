d5858bf13989896e70308a113b7d23dfc7db69da
rikizeivald <86789273+rikizeivald@users.noreply.github.com>
2023-04-17 00:18:43 +0300

    Update PetRepository.java

--- spring-boot-h2-database-crud-master/src/main/java/com/bezkoder/spring/jpa/h2/repository/PetRepository.java
+++ spring-boot-h2-database-crud-master/src/main/java/com/bezkoder/spring/jpa/h2/repository/PetRepository.java
@@ -1,11 +1,7 @@
 package com.bezkoder.spring.jpa.h2.repository;
-
-
-        import java.util.List;
-
-        import com.bezkoder.spring.jpa.h2.model.Pet;
-        import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
+import com.bezkoder.spring.jpa.h2.model.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PetRepository extends JpaRepository<Pet, Long> {
 }
